0bb692b8238aa371b5e638537c2eaa5c9679e7a2
Leon Sun <38485966+LeSun54@users.noreply.github.com>
2018-12-13 11:59:16 +0800

    Update NumPy.py

--- NumPy.py
+++ NumPy.py
@@ -128,3 +128,38 @@ np_baseball = np.array(baseball)
 print(np_baseball.shape)
 
 9.
+# baseball is available as a regular list of lists
+
+# Import numpy package
+import numpy as np
+
+# Create np_baseball (2 cols)
+np_baseball = np.array(baseball)
+
+# Print out the 50th row of np_baseball
+print(np_baseball[49,:])
+
+# Select the entire second column of np_baseball: np_weight_lb
+np_weight_lb = np_baseball[:,1]
+
+# Print out height of 124th player
+print(np_baseball[123, 0])
+
+10.
+# baseball is available as a regular list of lists
+# updated is available as 2D numpy array
+
+# Import numpy package
+import numpy as np
+
+# Create np_baseball (3 cols)
+np_baseball = np.array(baseball)
+
+# Print out addition of np_baseball and updated
+print(np_baseball + updated)
+
+# Create numpy array: conversion
+conversion = np.array([0.0254, 0.453592, 1])
+
+# Print out product of np_baseball and conversion
+print(np_baseball * conversion)
